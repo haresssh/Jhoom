87e39158eefd125492f67ff81d2482718a995f35
Haresh Prajapati <hp494343@gmail.com>
2026-06-08 23:08:55 +0530

    feat: use HTTPS with self-signed certificate for local testing on mobile

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,1 @@
-export const API_BASE_URL = 
-  window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:8080'
-    : `http://${window.location.hostname}:8080`;
+export const API_BASE_URL = '';
